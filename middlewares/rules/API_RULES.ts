e3e0309d352829c966ceadf62b8273d4a3fb5081
riahimedyassin <riahimohamedyassin@charguia.r-iset.tn>
2023-12-22 02:42:07 +0100

    Organized Server.ts

---
+++ middlewares/rules/API_RULES.ts
@@ -0,0 +1,14 @@
+import { Request, Response, NextFunction } from "express";
+
+export const API_RULES = (req: Request, res: Response, next: NextFunction) => {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "*");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Content-Type, Authorization, Origin, X-Requested-With, Accept"
+  );
+  if (req.method == "OPTIONS") {
+    res.header("Access-Control-Allow-Methods", "PUT, PATCH, POST, GET, DELETE");
+  }
+  next();
+};
--- server.ts
+++ server.ts
@@ -4,29 +4,52 @@ import { connectToDB } from "./config/connect";
 import { router as ProductRoute } from "./routes/Product.route";
 import { router as CompanyRoute } from "./routes/Company.route";
 import { ErrorHandler } from "./error/handler/ErrorHandler";
-
-dotenv.config();
+import cors from "cors";
+import { API_RULES } from "./middlewares/rules/API_RULES";
+import { BASE_URL } from "./constants/GENERAL";
 
 const app = express();
-const PORT = process.env.PORT || 3000;
-const CONENCTION_STRING = process.env.CONNECTION_STRING;
-const BASE_URL = "/api/asuna/v1";
+
+/*
+  CONFIG
+*/
+dotenv.config();
+cors({
+  origin: "*",
+});
+
+/*
+    API RULES
+*/
+app.use(API_RULES);
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
 /*
     ROUTES
 */
 
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
-
 app.use(`${BASE_URL}/products`, ProductRoute);
 app.use(`${BASE_URL}/companies`, CompanyRoute);
 app.use(ErrorHandler);
 
+/*
+  ENV
+*/
+const PORT = process.env.PORT || 3000;
+const CONENCTION_STRING = process.env.CONNECTION_STRING;
+
+/*
+    RUN
+*/
+
 const start = async () => {
   try {
-    if (typeof CONENCTION_STRING == "string") {
-      const connected = await connectToDB(CONENCTION_STRING);
+    if (
+      typeof CONENCTION_STRING == "string" &&
+      CONENCTION_STRING.trim() != ""
+    ) {
+      await connectToDB(CONENCTION_STRING);
       app.listen(PORT, () => {
         console.info(`[SERVER] : Server Running on PORT ${PORT}`);
       });
